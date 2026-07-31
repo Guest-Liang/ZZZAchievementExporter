2aeb4d25a46440b7e98f432f9ac3617b47fdcbbc
GuestLiang <GuestLiang@outlook.com>
2026-07-31 14:01:24 +0100

    🏷️ 更新3.1元数据

--- src/ZZZae.App/ApplicationBuildInfo.cs
+++ src/ZZZae.App/ApplicationBuildInfo.cs
@@ -2,7 +2,7 @@ namespace ZZZae.App;
 
 internal static class ApplicationBuildInfo
 {
-    public const string Version = "1.8.0";
+    public const string Version = "1.8.1";
 
 #if DEBUG
     public const string Configuration = "Debug";
